4f4e103c72c38b0a206cce1010ac1ca4f67afd9c
Andrew <zinn6885@pacificu.edu>
2024-10-07 19:12:42 -0700

    initializing

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write("This is a test element")
+# st.title("AI web Scraper")
+# url = st.text_input("Enter a Website URL: ")
+
+# if st.button("Scrape Site"):
+#     st.write("Scraping the website")
